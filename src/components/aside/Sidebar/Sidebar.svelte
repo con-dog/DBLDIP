13b6d8be3ed73726c3de112390c5d90a410a7cbb
connor <connordanielfrank@gmail.com>
2023-03-09 20:50:50 +1300

    dev: Add components to Sidebar

--- src/components/aside/Sidebar/Sidebar.svelte
+++ src/components/aside/Sidebar/Sidebar.svelte
@@ -4,7 +4,6 @@
     faCamera,
     faCircleHalfStroke,
     faCrop,
-    faPlus,
     faVideo
   } from '@fortawesome/free-solid-svg-icons'
 </script>
@@ -12,6 +11,67 @@
 <aside>
   <img src="/images/logo.png" alt="logo" />
   <button>
-    <Fa icon={faPlus} />
+    <Fa icon={faCircleHalfStroke} />
+  </button>
+  <button>
+    <Fa icon={faCrop} />
+  </button>
+  <button>
+    <Fa icon={faCamera} />
+  </button>
+  <button class="on">
+    <Fa icon={faVideo} />
   </button>
 </aside>
+
+<style lang="scss">
+  $rose: #f72585;
+  $grape: #7209b7;
+  $zaffre: #3a0ca3;
+  $neon-blue: #4361ee;
+  $vivid-sky-blue: #4cc9f0;
+  $offwhite: #f3f3f3;
+
+  aside {
+    position: fixed;
+    top: calc(50% - 300px);
+    left: 2rem;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 1rem;
+    width: 5rem;
+    height: 600px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
+    img {
+      top: 0;
+      margin-bottom: auto;
+      width: 5rem;
+      height: 5rem;
+      margin-top: 1rem;
+    }
+    button {
+      width: 5rem;
+      height: 5rem;
+      background-color: transparent;
+      color: #000;
+      border: none;
+      font-size: 1.75rem;
+      cursor: pointer;
+      transition: all 0.1s ease-in-out;
+      &:hover {
+        transform: scale(1.3);
+      }
+      &:active {
+        transform: scale(0.9);
+      }
+      &:focus {
+        color: $rose;
+      }
+    }
+  }
+</style>
