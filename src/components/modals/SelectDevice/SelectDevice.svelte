10e690d670a5c41207b16b95ead678173e9fdffb
connor <connordanielfrank@gmail.com>
2023-03-10 17:26:54 +1300

    feat: Add custom checkbox for selecting devices

--- src/components/modals/SelectDevice/SelectDevice.svelte
+++ src/components/modals/SelectDevice/SelectDevice.svelte
@@ -1,6 +1,133 @@
-<dialog />
+<script lang ts>
+  import FrameIphone12 from '../../devices/phones/Iphone12/FrameIphone12.svelte'
+
+  let size = 'thumbnail'
+</script>
+
+<dialog>
+  <span>1</span>
+  <p>2</p>
+  <form method="dialog">
+    <h1>Select devices to emulate</h1>
+    <div class="container">
+      <label>
+        <input type="checkbox" />
+        <div>
+          <span>iPhone 12</span>
+          <FrameIphone12 {size} />
+        </div>
+      </label>
+      <label>
+        <input type="checkbox" />
+        <div>
+          <span>iPhone 12</span>
+          <FrameIphone12 {size} />
+        </div>
+      </label>
+      <label>
+        <input type="checkbox" />
+        <div>
+          <span>iPhone 12</span>
+          <FrameIphone12 {size} />
+        </div>
+      </label>
+      <label>
+        <input type="checkbox" />
+        <div>
+          <span>iPhone 12</span>
+          <FrameIphone12 {size} />
+        </div>
+      </label>
+      <label>
+        <input type="checkbox" />
+        <div>
+          <span>iPhone 12</span>
+          <FrameIphone12 {size} />
+        </div>
+      </label>
+      <label>
+        <input type="checkbox" />
+        <div>
+          <span>iPhone 12</span>
+          <FrameIphone12 {size} />
+        </div>
+      </label>
+    </div>
+    <div>
+      <button type="reset">Cancel</button>
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+</dialog>
 
 <style lang="scss">
+  $hover-blue: hwb(185 97% 0%);
+  $rose: #f72585;
+  $grape: #7209b7;
+  $zaffre: #3a0ca3;
+  $neon-blue: #4361ee;
+  $vivid-sky-blue: #4cc9f0;
+  $offwhite: #f3f3f3;
+
+  label {
+    margin: 0;
+    padding: 0;
+  }
+  input[type='checkbox'] {
+    margin: 0;
+    padding: 0;
+    width: 0px;
+    height: 0px;
+    padding: 0;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    + div {
+      display: flex;
+      margin: 0;
+      margin-top: -18px;
+      padding: 0;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 0.5rem;
+      border: 1px solid black;
+      border-radius: 0.5rem;
+      width: 80px;
+      height: 80px;
+      background-color: white;
+      cursor: pointer;
+      transition: all 0.05s ease-in-out;
+      &:hover {
+        background-color: $offwhite;
+      }
+    }
+    &:focus {
+      + div {
+        outline: 1px solid black;
+      }
+    }
+    &:checked {
+      + div {
+        outline: 3px solid $vivid-sky-blue;
+        color: $rose;
+        font-weight: 700;
+        border: 1px solid transparent;
+      }
+    }
+  }
+
   dialog {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 300px;
+  }
+  .container {
+    margin: 2rem 0;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
   }
 </style>
