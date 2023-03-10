4142c79e6e61cc0ecf5e68c3c1f3df7e29eaa8d3
connor <connordanielfrank@gmail.com>
2023-03-10 17:25:54 +1300

    dev: Remove unused import

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Sidebar from './components/aside/Sidebar/Sidebar.svelte'
   import AddDevice from './components/buttons/AddDevice/AddDevice.svelte'
-  import Iphone12 from './components/devices/phones/Iphone12/Iphone12.svelte'
 </script>
 
 <main>
